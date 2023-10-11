0ad347e2c10bcf63012ad2a56c027a2a2c80d771
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-10-11 15:02:07 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="read-the-docs">
+       Coding By Cora is under construction till Nov 11th, 2023!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
