502db49a9ba95bc93f59f056f46f34a0f990620a
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-11-09 14:44:06 -0800

    changed date

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className="read-the-docs">
-        We are under construction till Nov 11th, 2023.
+        We are under construction till Nov 18th, 2023.
       </h1>
       <h2>Need help building your website? Please feel free to email <a href="mailto:codingbycora@gmail.com">CodingByCora@gmail.com</a></h2>
     </>
